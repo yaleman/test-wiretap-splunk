ecccd0d6f4a789ed4a28c960b7dcf13d98711c4f
James Hodgkinson <james@terminaloutcomes.com>
2024-05-01 13:26:45 +1000

    adding docker-compose to test nginx

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ docker run --platform linux/x86_64 \
     -e SPLUNK_START_ARGS=--accept-license \
     -e SPLUNK_PASSWORD=HappyPuppy123 \
     -p 8000:8000 \
-    --mount "type=bind,src=./default.yml,target=/tmp/defaults/default.yml" \
+    --name splunk \
     --rm -it \
     splunk/splunk:9.2.0
